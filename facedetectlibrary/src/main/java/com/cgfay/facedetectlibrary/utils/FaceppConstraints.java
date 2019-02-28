ccd9540bd7b938d20dc5edc858c10b0d98f8be93
huping <pinghu@deepglint.com>
2019-02-27 22:50:18 -0800

    checkout feature sdk

--- facedetectlibrary/src/main/java/com/cgfay/facedetectlibrary/utils/FaceppConstraints.java
+++ facedetectlibrary/src/main/java/com/cgfay/facedetectlibrary/utils/FaceppConstraints.java
@@ -2,8 +2,8 @@ package com.cgfay.facedetectlibrary.utils;
 
 public class FaceppConstraints {
 
-    public static String API_KEY = "1EDigZoAg7lFxzvP3PAXKff7Ka6B6gqW";
-    public static String API_SECRET = "QNkV6RkhbPjhgI0poSRKmCs7ay1Aishi";
+    public static String API_KEY = "jxbsJ3nOOof8KeM2GmEL54jzVHJUFPCU";
+    public static String API_SECRET = "qq3HcKaGDOucSLAI7SOXZ8Ru2avsWHcS";
 
     public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
     public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
